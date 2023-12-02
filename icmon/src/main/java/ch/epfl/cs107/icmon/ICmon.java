aac29d6d4d997cac794f5e490a1331a40e93b85b
N4dZ <dingweifeng.dw@gmail.com>
2023-12-02 19:38:02 +0100

    le meme push mais en ayant changé quelque merdes que j'ai fait en pushant

--- icmon/src/main/java/ch/epfl/cs107/icmon/ICmon.java
+++ icmon/src/main/java/ch/epfl/cs107/icmon/ICmon.java
@@ -65,7 +65,6 @@ public final class ICmon extends AreaGame {
         player = new ICMonPlayer(area, Orientation.DOWN, coords, "actors/player");
         player.enterArea(area, coords);
         player.centerCamera();
-        player.leaveArea();
     }
 
 //    private void switchArea() {
--- icmon/src/main/java/ch/epfl/cs107/icmon/area/maps/ICMonBehavior.java
+++ icmon/src/main/java/ch/epfl/cs107/icmon/area/maps/ICMonBehavior.java
@@ -17,8 +17,8 @@ public final class ICMonBehavior extends AreaBehavior {
      * @param name   (String): name of the behavior image, not null
      */
     public ICMonBehavior(Window window, String name) {
-        sut height = getHeight();
-        inper(window, name);
+        super(window, name);
+        int height = getHeight();
         int width = getWidth();
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
